8bd26c5f2163e7fe2df79e271c7ef9b10767785b
IAmEvyatar <evyatarsilberschatz@gmail.com>
2019-04-16 13:30:35 +0300

    adds basic ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+export interface AppState {data: string;}
 
-class App extends Component {
+class App extends Component<any, any> {
+  constructor(props: any){
+    super(props)
+    this.state = {
+    data: "default"
+    }
+  }
   render() {
     return (
-    );
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-12">
+            <header className="head-section">
+              <h1>This is Evyatar's React Wiki search!</h1>
+              {/* This is the searchbar area */}
+            </header>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12">
+            <main className="content-section">
+              {/* This is the search results area */}
+            </main>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
