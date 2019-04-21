905f479e966d733802844ba99ff9e19dc0ee85dd
IAmEvyatar <evyatarsilberschatz@gmail.com>
2019-04-21 22:15:47 +0300

    adds functionality to SearchResults

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -5,9 +5,15 @@ interface ISearchResultsProps {data:any}
 export class SearchResults extends Component<ISearchResultsProps> {
     render(){
         if(this.props.data.query){
-            const a = this.props.data.query.search.map((el: any)=>{return(
-                <div>
-                    {el.title}
+            const a = this.props.data.query.search.map((el: any,i:number)=>{
+                const URLParameter = el.title.split(" ").join("_")
+                const titleURL = `https://en.wikipedia.org/wiki/${URLParameter}`
+                return(
+                <div key={i}>
+                    <h2><a target="_blank" href={titleURL}>{el.title}</a><span className="badge badge-primary">{el.wordcount}</span></h2>
+                    <p dangerouslySetInnerHTML={{
+                        __html:el.snippet
+                    }}></p>
                 </div>
             )})
             return(
